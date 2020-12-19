36851b703ca6b569af08908dad79cae3d4104474
Daniel Cumberbatch <daniel.cumberbatch1@gmail.com>
2020-12-19 23:30:58 +0000

    Week1: Practice Assignment: Extended EventType with Coordinate data member specifying the location of the event.

--- Week1/Assignment/LineSegmentSweep/src/Event.java
+++ Week1/Assignment/LineSegmentSweep/src/Event.java
@@ -1,24 +1,21 @@
-enum EventType {
-	START,
-	END,
-	INTERSECTION,
-	UNKNOWN
-}
 
 public class Event {
-	public EventType type = EventType.UNKNOWN;
-	public LineSegment ls1 = null;
-	public LineSegment ls2 = null;
+	public EventType type;
+	public Coordinate coord;
+	public LineSegment ls1;
+	public LineSegment ls2;
 
-	public Event(EventType type_, LineSegment ls1_, LineSegment ls2_) {
+	public Event(EventType type_, Coordinate coord_, LineSegment ls1_, LineSegment ls2_) {
 		this.type = type_;
+		this.coord = coord_;
 		this.ls1 = ls1_;
 		this.ls2 = ls2_;
 	}
 
-	public Event(EventType type_, LineSegment ls1_) {
+	public Event(EventType type_, Coordinate coord_, LineSegment ls_) {
 		this.type = type_;
-		this.ls1 = ls1_;
+		this.coord = coord_;
+		this.ls1 = ls_;
 		this.ls2 = null;
 	}
 }

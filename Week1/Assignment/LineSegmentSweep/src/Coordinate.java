7e28669cd31fd8db80db0269b7cb387416cb7678
Daniel Cumberbatch <daniel.cumberbatch1@gmail.com>
2020-12-19 23:27:19 +0000

    Week1: Practice Assignment: Added class defining a Coordinate.

---
+++ Week1/Assignment/LineSegmentSweep/src/Coordinate.java
@@ -0,0 +1,24 @@
+public class Coordinate implements Comparable<Coordinate> {
+	public double x;
+	public double y;
+	
+	public Coordinate(double x_, double y_) {
+		this.x = x_;
+		this.y = y_;
+	}
+
+	@Override
+	public int compareTo(Coordinate o) {
+		if (this.y < o.y) {
+			return -1;
+		} else if (this.y > o.y) {
+			return +1;
+		} else if (this.x < o.x) {
+			return -1;
+		} else if (this.x > o.x) {
+			return +1;
+		} else {
+			return 0;
+		}
+	}
+}

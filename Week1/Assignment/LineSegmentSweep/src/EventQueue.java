d3570ad915bcbd04bcfd110b5b3b5e92080e0a69
Daniel Cumberbatch <daniel.cumberbatch1@gmail.com>
2020-12-19 23:47:01 +0000

    Week1: Practice Assignment: Implemented EventQueue.

--- Week1/Assignment/LineSegmentSweep/src/EventQueue.java
+++ Week1/Assignment/LineSegmentSweep/src/EventQueue.java
@@ -1,24 +1,28 @@
-
 public class EventQueue {
-	private BinarySearchTree bst = null;
-	
-	public EventQueue() {
-		this.bst = new BinarySearchTree();
-	}
-	
-	public void push(double key, Event e) {
-		this.bst.insert(key, e);
-	}
-	
-	public Event top() {
-		return this.bst.findMinimum().event;
-	}
-	
-	public void pop() {
-		this.bst.erase(this.bst.findMinimum().key);
-	}
-	
-	public boolean isEmpty() {
-		return this.bst.isEmpty();
-	}
+
+    private BinarySearchTree<Coordinate, Event> bst;
+
+    public EventQueue() {
+        bst = new BinarySearchTree<Coordinate, Event>();
+    }
+
+    public void enqueue(Event e) {
+        bst.insert(e.coord, e);
+    }
+
+    public Event dequeue() {
+        if (this.empty()) {
+            throw new IllegalStateException("dequeue - Cannot pop empty EventQueue.");
+        }
+
+        final Coordinate min = this.bst.minimum();
+        final Event minVal = this.bst.find(min);
+        this.bst.remove(min,null);
+
+        return minVal;
+    }
+
+    public boolean empty() {
+        return this.bst.empty();
+    }
 }

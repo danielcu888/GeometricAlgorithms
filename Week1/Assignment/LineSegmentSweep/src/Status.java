f2a31e40493368692fdc2079e6750198690e69cc
Daniel Cumberbatch <daniel.cumberbatch1@gmail.com>
2020-12-28 19:31:28 +0000

    Week1: Practice Assignment: Added missing documentation; Check for existence of predecessor/successor in Status methods and return null; throw if target node cannot be found.

--- Week1/Assignment/LineSegmentSweep/src/Status.java
+++ Week1/Assignment/LineSegmentSweep/src/Status.java
@@ -54,52 +54,116 @@ public class Status {
         }
     }
 
+    /**
+     * Remove the specified LineSegment from this Status.
+     * @param ls The LineSegment to be removed.
+     */
     public void remove(LineSegment ls) {
+
+        // Find the node associated with the specified LineSegment.
         final RetrieveNodeByValueVisitor v = new RetrieveNodeByValueVisitor(ls);
         this.bst.visit(v);
         final BinarySearchTreeNode<ComparableInteger, LineSegment> node = v.getNode();
+
+        // Remove the retrieved node.
         this.bst.remove(node);
     }
 
+    /**
+     * Retrieve the predecessor of the node with value equal to a
+     * specified LineSegment.
+     * @param ls The specified LineSegment.
+     * @return The predecessor of the node with value ls, or null if
+     *   there is no predecessor.
+     * @throws IllegalArgumentException if no node with value ls exists.
+     */
     public BinarySearchTreeNode<ComparableInteger, LineSegment> predecessor(LineSegment ls) {
-        /// @Take advantage that Status keys are unique, ordered and >= 0.
+
+        // Retrieve node with value ls.
         final RetrieveNodeByValueVisitor v = new RetrieveNodeByValueVisitor(ls);
         this.bst.visit(v);
         final BinarySearchTreeNode<ComparableInteger, LineSegment> node = v.getNode();
+
         if (node == null) {
+            throw new IllegalArgumentException("No node with specified LineSegment in Status.");
+        }
+
+        // Check there is a predecessor, taking advantage of the fact that
+        // the Status nodes have unique keys [0,1,2,3,...].
+        if (node.key.i == 0) {
+            // No predecessor.
             return null;
         }
+
+        // Retrieve the node with key value one less than that retrieved.
         final ComparableInteger key = new ComparableInteger(node.key.i-1);
         return this.bst.findNode(key, null);
     }
 
+    /**
+     * Retrieve the successor of the node with value equal to a
+     * specified LineSegment.
+     * @param ls The specified LineSegment.
+     * @return The successor of the node with value ls, or null if
+     *   there is no successor.
+     * @throws IllegalArgumentException if no node with value ls exists.
+     */
     public BinarySearchTreeNode<ComparableInteger, LineSegment> successor(LineSegment ls) {
-        /// @Take advantage that Status keys are unique, ordered and >= 0.
+
+        // Retrieve node with value ls
         final RetrieveNodeByValueVisitor v = new RetrieveNodeByValueVisitor(ls);
         this.bst.visit(v);
         final BinarySearchTreeNode<ComparableInteger, LineSegment> node = v.getNode();
+
         if (node == null) {
+            throw new IllegalArgumentException("No node with specified LineSegment in Status.");
+        }
+
+        // Check there is a successor, taking advantage of the fact that
+        // the Status nodes have unique keys [0,1,2,3,...].
+        if (node.key.i+1 == this.bst.size()) {
+            // No successor
             return null;
         }
+
+        // Retrieve the node with key value one greater than that retrieved.
         final ComparableInteger key = new ComparableInteger(node.key.i+1);
         return this.bst.findNode(key, null);
     }
 
+    /**
+     * Swap the values of the nodes whose values are equal to the specified
+     * LineSegments.
+     * @param ls1 The first LineSegment.
+     * @param ls2 The second LineSegment.
+     * @throws IllegalArgumentException if a node with value equal to either
+     *   ls1 or ls2 could not be found.
+     */
     public void swap(LineSegment ls1, LineSegment ls2) {
+
+        // Retrieve the node with value, ls1.
+
         final RetrieveNodeByValueVisitor v1 = new RetrieveNodeByValueVisitor(ls1);
         this.bst.visit(v1);
+
         final BinarySearchTreeNode<ComparableInteger, LineSegment> n1 = v1.getNode();
+
         if (n1 == null) {
-            throw new IllegalStateException("swap - Node with value ls1 not found.");
+            throw new IllegalArgumentException("swap - Node with value ls1 not found.");
         }
+
+        // Retrieve the node with value, ls2.
 
         final RetrieveNodeByValueVisitor v2 = new RetrieveNodeByValueVisitor(ls2);
         this.bst.visit(v2);
+
         final BinarySearchTreeNode<ComparableInteger, LineSegment> n2 = v2.getNode();
+
         if (n2 == null) {
-            throw new IllegalStateException("swap - Node with value ls2 not found.");
+            throw new IllegalArgumentException("swap - Node with value ls2 not found.");
         }
 
+        // Swap the values of the retrieved nodes.
         final LineSegment ls = n1.value;
         n1.value = n2.value;
         n2.value = ls;

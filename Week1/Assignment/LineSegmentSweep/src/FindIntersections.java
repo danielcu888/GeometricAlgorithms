c54edab397f8dade3e4453adb6a23ef7f97eccf3
Daniel Cumberbatch <daniel.cumberbatch1@gmail.com>
2020-12-30 02:44:26 +0000

    Week1: Practice Assignment: FindIntersections: Re-ordered method definitions; Added documentation; Used new EventType fields; Remove duplicates intersection Events.

--- Week1/Assignment/LineSegmentSweep/src/FindIntersections.java
+++ Week1/Assignment/LineSegmentSweep/src/FindIntersections.java
@@ -2,64 +2,34 @@ import java.util.ArrayList;
 
 public class FindIntersections {
 
-    ArrayList<Event> inx;
+    // Private data members
+    private ArrayList<Event> inx;
 
-    FindIntersections(ArrayList<LineSegment> segments) {
-        this.inx = new ArrayList<Event>();
+    // Private methods
 
-        // Construct empty EventQueue.
-        final EventQueue q = new EventQueue();
-
-        // Populate the EventQueue with the START and END Events.
-        for (LineSegment ls : segments) {
-            q.enqueue(new Event(EventType.START, ls.start, ls));
-            q.enqueue(new Event(EventType.END, ls.end, ls));
-        }
-
-        // Construct Status.
-        final Status s = new Status();
-
-        // Process Events.
-        while (!q.empty()) {
-
-            // Retrieve next Event.
-            final Event e = q.dequeue();
-
-            // Switch on type of current Event.
-            final EventType et = e.type;
-            switch (et) {
-            case START:
-                this.handleStartEvent(e, q, s);
-                break;
-            case END:
-                this.handleEndEvent(e, q, s);
-                break;
-            case INTERSECTION:
-                this.handleInxEvent(e, q, s);
-                break;
-            default:
-                throw new IllegalArgumentException("Unrecognised EventType: " + et);
-            }
-        }
-    }
-
-    ArrayList<Event> intersections() {
-        return this.inx;
-    }
-
+    /**
+     * Process a specified StartEvent.
+     * @param e The START Event to be processed.
+     * @param q The EventQueue from which the Event was dequeued.
+     * @param s The Status managing the LineSegments associated
+     *   with the sweep pertaining to the specified Event.
+     */
     private void handleStartEvent(Event e, EventQueue q, Status s) {
+
         // 1. Insert the LineSegment into the Status.
         s.insert(e.ls1);
 
-        // 2. Retrieve the neighbouring LineSegments of the new LineSegment.
+        // 2. Retrieve the neighbouring LineSegments of the new LineSegment
+        //    currently in the Status.
+
         final BinarySearchTreeNode<ComparableInteger, LineSegment> pred
             = s.predecessor(e.ls1);
 
         final BinarySearchTreeNode<ComparableInteger, LineSegment> succ
             = s.successor(e.ls1);
 
-        // 3. Check for future intersections between the new LineSegment and its
-        //    neighbours.
+        // 3. Check for future intersections between the new LineSegment and
+        //    those retrieved.
 
         InxData pred_inx = null;
         if (pred != null) {
@@ -71,18 +41,15 @@ public class FindIntersections {
             succ_inx = LineSegment.intersection(e.ls1, succ.value);
         }
 
-        // 4. If any intersections are found, add the found intersections as Events
-        //    to the EventQueue.
+        // 4. If any intersections are found, enqueue them as Events.
 
         if (pred_inx != null) {
             switch (pred_inx.dim) {
             case POINT:
-                q.enqueue(new Event(EventType.INTERSECTION, pred_inx.coord, e.ls1, pred.value));
+                q.enqueue(new Event(EventType.POINT_INTERSECTION, pred_inx.coord, e.ls1, pred.value));
                 break;
             case LINE:
-                /**
-                 * @todo - Process line intersections.
-                 */
+                q.enqueue(new Event(EventType.LINE_INTERSECTION, pred_inx.coord, e.ls1, pred.value));
                 break;
             case UNKNOWN:
                 throw new IllegalStateException("Did not expect UNKNOWN InxDim.");
@@ -94,12 +61,10 @@ public class FindIntersections {
         if (succ_inx != null) {
             switch (succ_inx.dim) {
             case POINT:
-                q.enqueue(new Event(EventType.INTERSECTION, succ_inx.coord, e.ls1, succ.value));
+                q.enqueue(new Event(EventType.POINT_INTERSECTION, succ_inx.coord, e.ls1, succ.value));
                 break;
             case LINE:
-                /**
-                 * @todo - Process line intersections.
-                 */
+                q.enqueue(new Event(EventType.LINE_INTERSECTION, succ_inx.coord, e.ls1, succ.value));
                 break;
             case UNKNOWN:
                 throw new IllegalStateException("Did not expect UNKNOWN InxDim.");
@@ -109,9 +74,16 @@ public class FindIntersections {
         }
     }
 
+    /**
+     * Process a specified EndEvent.
+     * @param e The END Event to be processed.
+     * @param q The EventQueue from which the Event was dequeued.
+     * @param s The Status managing the LineSegments associated
+     *   with the sweep pertaining to the specified Event.
+     */
     private void handleEndEvent(Event e, EventQueue q, Status s) {
 
-        // 1. Retrieve the neighbours of the LineSegment to be removed.
+        // 1. Retrieve the Status neighbours of the LineSegment to be removed.
 
         final BinarySearchTreeNode<ComparableInteger, LineSegment> pred
             = s.predecessor(e.ls1);
@@ -122,24 +94,22 @@ public class FindIntersections {
         // 2. Remove the LineSegment from the Status.
         s.remove(e.ls1);
 
-        // 3. Check for future intersections involving the neighbours of the
+        // 3. Check for intersections between the Status neighbours of the
         //    removed LineSegment.
         InxData inx = null;
         if ((pred != null) && (succ != null)) {
             inx = LineSegment.intersection(pred.value, succ.value);
         }
 
-        // 4. If any intersections are found then add them as Event to the
+        // 4. If an intersection is found then add it as an Event to the
         //    EventQueue.
         if (inx != null) {
             switch (inx.dim) {
             case POINT:
-                q.enqueue(new Event(EventType.INTERSECTION, inx.coord, pred.value, succ.value));
+                q.enqueue(new Event(EventType.POINT_INTERSECTION, inx.coord, pred.value, succ.value));
                 break;
             case LINE:
-                /**
-                 * @todo - Process line intersections.
-                 */
+                q.enqueue(new Event(EventType.LINE_INTERSECTION, inx.coord, pred.value, succ.value));
                 break;
             case UNKNOWN:
                 throw new IllegalStateException("Did not expect UNKNOWN InxDim.");
@@ -149,31 +119,42 @@ public class FindIntersections {
         }
     }
 
+    /**
+     * Process a specified Intersection Event.
+     * @param e The *_INTERSECTION Event to be processed.
+     * @param q The EventQueue from which the Event was dequeued.
+     * @param s The Status managing the LineSegments associated
+     *   with the sweep pertaining to the specified Event.
+     */
     private void handleInxEvent(Event e, EventQueue q, Status s) {
-        // 1. Add the Event to this.inx.
+
+        // 1. Add the Event to the output collection.
         this.inx.add(e);
 
         // 2. Swap the order of the two associated LineSegments in the
         //    Status.
         s.swap(e.ls1, e.ls2);
 
-        // 3. For each of the two modified LineSegments, check if their
-        //    new neighbours intersect with them in the future,and if found
-        //    add to the EventQueue.
+        // 3. For each of the two intersecting LineSegments, check if their
+        //    new Status neighbours intersect with them, and, if so, enqueue
+        //    the intersections as Events to the EventQueue, with ls1 of that
+        //    Event being the LineSegment with the smaller id.
 
         final BinarySearchTreeNode<ComparableInteger, LineSegment> pred1
             = s.predecessor(e.ls1);
+
         if (pred1 != null) {
             final InxData inx = LineSegment.intersection(e.ls1, pred1.value);
 
+            final LineSegment ls1 = (e.ls1.id < pred1.value.id) ? e.ls1 : pred1.value;
+            final LineSegment ls2 = (ls1 != e.ls1) ? e.ls1 : pred1.value;
+
             switch (inx.dim) {
             case POINT:
-                q.enqueue(new Event(EventType.INTERSECTION, inx.coord, e.ls1, pred1.value));
+                q.enqueue(new Event(EventType.POINT_INTERSECTION, inx.coord, ls1, ls2));
                 break;
             case LINE:
-                /**
-                 * @todo - Process line intersections.
-                 */
+                q.enqueue(new Event(EventType.LINE_INTERSECTION, inx.coord, ls1, ls2));
                 break;
             case UNKNOWN:
                 throw new IllegalStateException("Did not expect UNKNOWN InxDim.");
@@ -188,14 +169,15 @@ public class FindIntersections {
         if (suc1 != null) {
             final InxData inx = LineSegment.intersection(e.ls1, suc1.value);
 
+            final LineSegment ls1 = (e.ls1.id < suc1.value.id) ? e.ls1 : suc1.value;
+            final LineSegment ls2 = (ls1 != e.ls1) ? e.ls1 : suc1.value;
+
             switch (inx.dim) {
             case POINT:
-                q.enqueue(new Event(EventType.INTERSECTION, inx.coord, e.ls1, suc1.value));
+                q.enqueue(new Event(EventType.POINT_INTERSECTION, inx.coord, ls1, ls2));
                 break;
             case LINE:
-                /**
-                 * @todo - Process line intersections.
-                 */
+                q.enqueue(new Event(EventType.LINE_INTERSECTION, inx.coord, ls1, ls2));
                 break;
             case UNKNOWN:
                 throw new IllegalStateException("Did not expect UNKNOWN InxDim.");
@@ -210,14 +192,15 @@ public class FindIntersections {
         if (pred2 != null) {
             final InxData inx = LineSegment.intersection(e.ls2, pred2.value);
 
+            final LineSegment ls1 = (e.ls2.id < pred2.value.id) ? e.ls2 : pred2.value;
+            final LineSegment ls2 = (ls1 != e.ls2) ? e.ls2 : pred2.value;
+
             switch (inx.dim) {
             case POINT:
-                q.enqueue(new Event(EventType.INTERSECTION, inx.coord, e.ls2, pred2.value));
+                q.enqueue(new Event(EventType.POINT_INTERSECTION, inx.coord, ls1, ls2));
                 break;
             case LINE:
-                /**
-                 * @todo - Process line intersections.
-                 */
+                q.enqueue(new Event(EventType.LINE_INTERSECTION, inx.coord, ls1, ls2));
                 break;
             case UNKNOWN:
                 throw new IllegalStateException("Did not expect UNKNOWN InxDim.");
@@ -232,14 +215,15 @@ public class FindIntersections {
         if (suc2 != null) {
             final InxData inx = LineSegment.intersection(e.ls2, suc2.value);
 
+            final LineSegment ls1 = (e.ls2.id < suc2.value.id) ? e.ls2 : suc2.value;
+            final LineSegment ls2 = (ls1 != e.ls2) ? e.ls2 : suc2.value;
+
             switch (inx.dim) {
             case POINT:
-                q.enqueue(new Event(EventType.INTERSECTION, inx.coord, e.ls2, suc2.value));
+                q.enqueue(new Event(EventType.POINT_INTERSECTION, inx.coord, ls1, ls2));
                 break;
             case LINE:
-                /**
-                 * @todo - Process line intersections.
-                 */
+                q.enqueue(new Event(EventType.POINT_INTERSECTION, inx.coord, ls1, ls2));
                 break;
             case UNKNOWN:
                 throw new IllegalStateException("Did not expect UNKNOWN InxDim.");
@@ -247,5 +231,110 @@ public class FindIntersections {
                 throw new IllegalStateException("Unrecognised InxDim");
             }
         }
+    }
+
+    // Public constructors
+
+    /**
+     * Construct using a collection of LineSegments whose
+     * intersections are to be found.
+     * @param segments A collection of LineSegments whose
+     *   intersections are to be found.
+     */
+    FindIntersections(ArrayList<LineSegment> segments) {
+
+        // Construct empty list to store the Events of the sweep.
+        this.inx = new ArrayList<Event>();
+
+        // Construct empty EventQueue to manage the Events.
+        final EventQueue q = new EventQueue();
+
+        // Populate the EventQueue with the START and END Events.
+        for (LineSegment ls : segments) {
+
+            if (ls == null) {
+                throw new IllegalArgumentException("null LineSegment.");
+            }
+
+            // Clone and reverse any LineSegment that has dy/dx <= 0.
+
+            final double grad = ls.gradient();
+            if ((grad == Double.NEGATIVE_INFINITY) || grad < 0) {
+                // Clone and reverse.
+                ls = new LineSegment(ls.id,ls.end,ls.start);
+            }
+
+            // Add the endpoint events for the current LineSegment.
+            q.enqueue(new Event(EventType.START, ls.start, ls));
+            q.enqueue(new Event(EventType.END, ls.end, ls));
+        }
+
+        // Construct empty Status.
+        final Status s = new Status();
+
+        // Process Events at increasing y values.
+        while (!q.empty()) {
+
+            // Retrieve next Event.
+            final Event e = q.dequeue();
+
+            // Switch on type of current Event.
+            final EventType et = e.type;
+            switch (et) {
+            case START:
+                this.handleStartEvent(e, q, s);
+                break;
+            case END:
+                this.handleEndEvent(e, q, s);
+                break;
+            case POINT_INTERSECTION:
+                // Fallthrough.
+            case LINE_INTERSECTION:
+                this.handleInxEvent(e, q, s);
+                break;
+            default:
+                throw new IllegalArgumentException("Unrecognised EventType: " + et);
+            }
+        }
+
+        if (!this.inx.isEmpty()) {
+            // Remove duplicate intersection Events.
+
+            // 1. sort the intersection events in ascending y-value.
+            this.inx.sort(new IntersectionEventComparator());
+
+            // 2. Iterate through Events with the same y and remove all Events with
+            //    that have the same LineSegment references. When an Event with a larger
+            //    y is found, repeat the process using that Event as the reference.
+            Event referenceEvent = null;
+            for (Event e : this.inx) {
+
+                // Check this Event is an intersection event.
+                if ((e.type != EventType.POINT_INTERSECTION) && (e.type != EventType.LINE_INTERSECTION)) {
+                    throw new IllegalStateException("Non-intersection Event found.");
+                }
+
+                if ((referenceEvent == null) || (referenceEvent.coord.y < e.coord.y)) {
+                    // Set new reference Event.
+                    referenceEvent = e;
+                } else if ((e.ls1 == referenceEvent.ls1) && (e.ls2 == referenceEvent.ls2)) {
+                    // There can only be 1 intersection Event involving the same 2 LineSegments - remove duplicate.
+                    this.inx.remove(e);
+                }
+            }
+        }
+    }
+
+    // Public methods
+
+    /**
+     * Return a collection of the intersection events associated with the
+     * LineSegments associated with this FindIntersections object.
+     * @return Return a collection of the intersection events associated
+     *   with the LineSegments associated with this FindIntersections
+     *   object.
+     */
+    public ArrayList<Event> intersections() {
+        return this.inx;
     }
 }

8c3a7a6b1f4c49fedab25544cb375f7ffa4eaf00
Daniel Cumberbatch <daniel.cumberbatch1@gmail.com>
2021-01-01 17:57:08 +0000

    Week1: Practice Assignment: Added Event.toString override and missing documentation for Event class.

--- Week1/Assignment/LineSegmentSweep/src/Event.java
+++ Week1/Assignment/LineSegmentSweep/src/Event.java
@@ -1,21 +1,55 @@
-
+/**
+ * A class to represent an event during a line sweep.
+ */
 public class Event {
-	public EventType type;
-	public Coordinate coord;
-	public LineSegment ls1;
-	public LineSegment ls2;
 
-	public Event(EventType type_, Coordinate coord_, LineSegment ls1_, LineSegment ls2_) {
-		this.type = type_;
-		this.coord = coord_;
-		this.ls1 = ls1_;
-		this.ls2 = ls2_;
-	}
+    // Public data members
 
-	public Event(EventType type_, Coordinate coord_, LineSegment ls_) {
-		this.type = type_;
-		this.coord = coord_;
-		this.ls1 = ls_;
-		this.ls2 = null;
-	}
+    public EventType type;
+    public Coordinate coord;
+    public LineSegment ls1;
+    public LineSegment ls2;
+
+    /**
+     * Four-parameter constuctor.
+     * @param type_ The type of the event.
+     * @param coord_ The 2D location of the event.
+     * @param ls1_ The first LineSegment associated with the event.
+     * @param ls2_ The second LineSegment associated with the event
+     *   (currently only acknowledged for intersection events).
+     */
+    public Event(EventType type_, Coordinate coord_, LineSegment ls1_, LineSegment ls2_) {
+        this.type = type_;
+        this.coord = coord_;
+        this.ls1 = ls1_;
+        this.ls2 = ls2_;
+    }
+
+    /**
+     * Three-parameter constuctor.
+     * @param type_ The type of the event.
+     * @param coord_ The 2D location of the event.
+     * @param ls1_ The LineSegment associated with the event.
+     */
+    public Event(EventType type_, Coordinate coord_, LineSegment ls_) {
+        this.type = type_;
+        this.coord = coord_;
+        this.ls1 = ls_;
+        this.ls2 = null;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append("Event{");
+        sb.append(this.type);
+        sb.append(",");
+        sb.append(this.coord);
+        sb.append(",");
+        sb.append(ls1);
+        sb.append(",");
+        sb.append(ls2);
+        sb.append("}");
+        return sb.toString();
+    }
 }

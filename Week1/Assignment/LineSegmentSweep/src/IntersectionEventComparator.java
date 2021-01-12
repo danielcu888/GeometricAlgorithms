8ef7afe9f0f18ab3cf8d9942e8319fbed6373c66
Daniel Cumberbatch <daniel.cumberbatch1@gmail.com>
2021-01-12 22:14:12 +0000

    Week1: Practice Assignment: Sort Events at equal y by x.

--- Week1/Assignment/LineSegmentSweep/src/IntersectionEventComparator.java
+++ Week1/Assignment/LineSegmentSweep/src/IntersectionEventComparator.java
@@ -1,17 +1,33 @@
 import java.util.Comparator;
 
+/**
+ * A Comparator subclass to compare Events, so that
+ * ascending order corresponds to descending ordinate
+ * value, first y, then x.
+ */
 public class IntersectionEventComparator implements Comparator<Event> {
 
     @Override
     public int compare(Event o1, Event o2) {
-        final double d1 = o1.coord.y;
-        final double d2 = o2.coord.y;
+        final double y1 = o1.coord.y;
+        final double y2 = o2.coord.y;
 
-        if (d1 < d2) {
+        if (y1 < y2) {
             return -1;
         }
 
-        if (d1 > d2) {
+        if (y1 > y2) {
+            return 1;
+        }
+
+        final double x1 = o1.coord.x;
+        final double x2 = o2.coord.x;
+
+        if (x1 < x2) {
+            return -1;
+        }
+
+        if (x1 > x2) {
             return 1;
         }
 

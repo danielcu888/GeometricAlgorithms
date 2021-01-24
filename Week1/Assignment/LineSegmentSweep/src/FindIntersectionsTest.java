1a48fd075de22a05ae6ef48476fc1406e636112d
Daniel Cumberbatch <daniel.cumberbatch1@gmail.com>
2021-01-24 02:55:12 +0000

    Week1: Practice Assignment: Updated test; Enabled all find intersections tests.

--- Week1/Assignment/LineSegmentSweep/src/FindIntersectionsTest.java
+++ Week1/Assignment/LineSegmentSweep/src/FindIntersectionsTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 
 class FindIntersectionsTest {
 
-/*
     @Test
     void testConstruct1() {
         final FindIntersections fi = new FindIntersections(new ArrayList<LineSegment>());
@@ -64,7 +63,6 @@ class FindIntersectionsTest {
         assertEquals(0, inx.ls2.start.compareTo(e2));
         assertEquals(0, inx.ls2.end.compareTo(s2));
     }
-*/
 
     @Test
     void testFindIntersectionsPairs() {
@@ -124,6 +122,58 @@ class FindIntersectionsTest {
         final Coordinate e11 = new Coordinate(128,290);
         final LineSegment ls11 = new LineSegment(11,s11,e11);
 
+        final Coordinate s12 = new Coordinate(722,649);
+        final Coordinate e12 = new Coordinate(638,243);
+        final LineSegment ls12 = new LineSegment(12,s12,e12);
+
+        final Coordinate s13 = new Coordinate(383,639);
+        final Coordinate e13 = new Coordinate(35,318);
+        final LineSegment ls13 = new LineSegment(13,s13,e13);
+
+        final Coordinate s14 = new Coordinate(654,626);
+        final Coordinate e14 = new Coordinate(810,543);
+        final LineSegment ls14 = new LineSegment(14,s14,e14);
+
+        final Coordinate s15 = new Coordinate(87,616);
+        final Coordinate e15 = new Coordinate(149,23);
+        final LineSegment ls15 = new LineSegment(15,s15,e15);
+
+        final Coordinate s16 = new Coordinate(308,601);
+        final Coordinate e16 = new Coordinate(828,75);
+        final LineSegment ls16 = new LineSegment(16,s16,e16);
+
+        final Coordinate s17 = new Coordinate(850,538);
+        final Coordinate e17 = new Coordinate(672,225);
+        final LineSegment ls17 = new LineSegment(17,s17,e17);
+
+        final Coordinate s18 = new Coordinate(314,491);
+        final Coordinate e18 = new Coordinate(247,283);
+        final LineSegment ls18 = new LineSegment(18,s18,e18);
+
+        final Coordinate s19 = new Coordinate(463,450);
+        final Coordinate e19 = new Coordinate(878,233);
+        final LineSegment ls19 = new LineSegment(19,s19,e19);
+
+        final Coordinate s20 = new Coordinate(215,444);
+        final Coordinate e20 = new Coordinate(206,148);
+        final LineSegment ls20 = new LineSegment(20,s20,e20);
+
+        final Coordinate s21 = new Coordinate(786,433);
+        final Coordinate e21 = new Coordinate(463,58);
+        final LineSegment ls21 = new LineSegment(21,s21,e21);
+
+        final Coordinate s22 = new Coordinate(760,353);
+        final Coordinate e22 = new Coordinate(91,179);
+        final LineSegment ls22 = new LineSegment(22,s22,e22);
+
+        final Coordinate s23 = new Coordinate(932,188);
+        final Coordinate e23 = new Coordinate(780,139);
+        final LineSegment ls23 = new LineSegment(23,s23,e23);
+
+        final Coordinate s24 = new Coordinate(701,107);
+        final Coordinate e24 = new Coordinate(364,127);
+        final LineSegment ls24 = new LineSegment(24,s24,e24);
+
         final ArrayList<LineSegment> segments = new ArrayList<LineSegment>();
         segments.add(ls0);
         segments.add(ls1);
@@ -137,60 +187,7 @@ class FindIntersectionsTest {
         segments.add(ls9);
         segments.add(ls10);
         segments.add(ls11);
-
-        /*
-        final Coordinate s13 = new Coordinate(722,649);
-        final Coordinate e13 = new Coordinate(638,243);
-        final LineSegment ls13 = new LineSegment(12,s13,e13);
-
-        final Coordinate s14 = new Coordinate(383,639);
-        final Coordinate e14 = new Coordinate(35,318);
-        final LineSegment ls14 = new LineSegment(13,s14,e14);
-
-        final Coordinate s15 = new Coordinate(654,626);
-        final Coordinate e15 = new Coordinate(810,543);
-        final LineSegment ls15 = new LineSegment(14,s15,e15);
-
-        final Coordinate s16 = new Coordinate(87,616);
-        final Coordinate e16 = new Coordinate(149,23);
-        final LineSegment ls16 = new LineSegment(15,s16,e16);
-
-        final Coordinate s17 = new Coordinate(308,601);
-        final Coordinate e17 = new Coordinate(828,75);
-        final LineSegment ls17 = new LineSegment(16,s17,e17);
-
-        final Coordinate s18 = new Coordinate(850,538);
-        final Coordinate e18 = new Coordinate(672,225);
-        final LineSegment ls18 = new LineSegment(17,s18,e18);
-
-        final Coordinate s19 = new Coordinate(314,491);
-        final Coordinate e19 = new Coordinate(247,283);
-        final LineSegment ls19 = new LineSegment(18,s19,e19);
-
-        final Coordinate s20 = new Coordinate(463,450);
-        final Coordinate e20 = new Coordinate(878,233);
-        final LineSegment ls20 = new LineSegment(19,s20,e20);
-
-        final Coordinate s21 = new Coordinate(215,444);
-        final Coordinate e21 = new Coordinate(206,148);
-        final LineSegment ls21 = new LineSegment(20,s21,e21);
-
-        final Coordinate s22 = new Coordinate(786,433);
-        final Coordinate e22 = new Coordinate(463,58);
-        final LineSegment ls22 = new LineSegment(21,s22,e22);
-
-        final Coordinate s23 = new Coordinate(760,353);
-        final Coordinate e23 = new Coordinate(91,179);
-        final LineSegment ls23 = new LineSegment(22,s23,e23);
-
-        final Coordinate s24 = new Coordinate(932,188);
-        final Coordinate e24 = new Coordinate(780,139);
-        final LineSegment ls24 = new LineSegment(23,s24,e24);
-
-        final Coordinate s25 = new Coordinate(701,107);
-        final Coordinate e25 = new Coordinate(364,127);
-        final LineSegment ls25 = new LineSegment(24,s25,e25);
-
+        segments.add(ls12);
         segments.add(ls13);
         segments.add(ls14);
         segments.add(ls15);
@@ -203,14 +200,12 @@ class FindIntersectionsTest {
         segments.add(ls22);
         segments.add(ls23);
         segments.add(ls24);
-        segments.add(ls25);
-        */
 
         final FindIntersections fi = new FindIntersections(segments);
         assertNotEquals(null, fi);
 
         final ArrayList<Event> inxs = fi.intersections();
         assertNotEquals(null, inxs);
-        assertEquals(57, inxs.size());
+        assertEquals(60, inxs.size());
     }
 }
